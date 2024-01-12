ba27720f21ec54b4463e6a1a09e0abe2bfa71588
nicopressez <154272808+nicopressez@users.noreply.github.com>
2024-01-12 13:00:01 +0100

    Fixed random turn if cell is already hit
    If the coordinates that were randomized have already been either hit or miss, then recurse random round

--- src/__tests__/player.test.js
+++ src/__tests__/player.test.js
@@ -15,9 +15,26 @@ describe("Player function tests", () => {
     player1.playTurn(player2, 0, 0);
     expect(player2.board.board[0][0]).toBe("hit");
   });
-  test('Computer plays random round and hits', () =>{
+  test('Computer plays random round again if cell is already hit/miss', () =>{
+    const mockRandom = jest.spyOn(Math, 'random');
+    const randomTurnSpy = jest.spyOn(player2, 'randomTurn')
 
+    // Mocking random numbers values for first turn
+    mockRandom.mockReturnValueOnce(0.8);
+    mockRandom.mockReturnValueOnce(0.8);
+
+    // Mocking random numbers values for first turn
+    mockRandom.mockReturnValueOnce(0.9);
+    mockRandom.mockReturnValueOnce(0.9);
+
+    // Board has already been hit for the decided values
+    player1.board.board[8][8] = "hit";
+    player1.board.board[9][9] = "hit";
+
+    player2.randomTurn(player1);
+    expect (randomTurnSpy).toHaveBeenCalledTimes(3);
     
+    mockRandom.mockRestore();  
   })
 
 });
--- src/player.js
+++ src/player.js
@@ -8,10 +8,15 @@ class Player {
     otherPlayer.board.receiveAttack(x, y);
   }
   randomTurn(otherPlayer ){
-    const randomX = Math.floor(Math.random()*10);
-    const randomY = Math.floor(Math.random()*10);
-    this.playTurn(otherPlayer, randomX,randomY);
-  }
-}
+    const randomX = () => Math.floor(Math.random()*10);
+    const randomY = () => Math.floor(Math.random()*10);
+    let x = randomX()
+    let y = randomY()
+
+    if (otherPlayer.board.board[x][y] !== "miss" && otherPlayer.board.board[x][y] !== "hit")
+   {this.playTurn(otherPlayer, x, y)}
+  else this.randomTurn(otherPlayer);
+  }}
+
 
 module.exports = { Player };
