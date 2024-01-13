60b4f26f7dc1a191a54f6644a8ffa924715ef179
nicopressez <154272808+nicopressez@users.noreply.github.com>
2024-01-13 17:21:27 +0100

    Added random computer ship placement

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import { Player } from "./player";
-import { Gameboard } from "./gameBoard";
+import { endGameUI } from "./dom";
 import { generateBoard } from "./dom";
 import { placeShipsGrid } from "./preGameDOM";
-
 
 export const player1 = new Player("player1");
 export const player2 = new Player("player2");
@@ -11,22 +10,44 @@ export const gameCheck = (otherPlayer, player) =>{
     if (otherPlayer.board.allSunk()) {
     endGameUI(player);
     return true;
-//    endGame();
     }
     else return false;
 }
 
 export const startGame = () => {
-
-
-player2.board.placeShip(player2.board.carrier, 0, 1);
-player2.board.placeShip(player2.board.battleship, 1, 1);
-player2.board.placeShip(player2.board.destroyer, 2, 3);
-player2.board.placeShip(player2.board.submarine, 3, 4);
-player2.board.placeShip(player2.board.patrolBoat, 4, 2);
-
+randomizeComputerShips();
 generateBoard(player1);
 generateBoard(player2);
 }
 
+const randomizeComputerShips = () => {
+
+const randomX = () => Math.floor(Math.random()*10);
+const randomY = () => Math.floor(Math.random()*10);
+
+const placeCarrier = () => {
+    if (player2.board.placeShip(player2.board.carrier, randomX(),randomY())) return true;
+    else return placeCarrier();
+}
+
+const placeBattleShip = () => {
+    if (player2.board.placeShip(player2.board.battleship, randomX(),randomY())) return true;
+    else return placeBattleShip();
+}
+
+const placeDestroyer = () => {
+    if (player2.board.placeShip(player2.board.destroyer, randomX(),randomY())) return true;
+    else return placeDestroyer();
+}
+
+const placeSubmarine = () => {
+    if (player2.board.placeShip(player2.board.submarine, randomX(),randomY())) return true;
+    else return placeSubmarine();
+}
+const placePatrolBoat = () => {
+    if (player2.board.placeShip(player2.board.patrolBoat, randomX(),randomY())) return true;
+    else return placePatrolBoat();
+}
+
+placeCarrier(); placeBattleShip(); placeDestroyer(); placeSubmarine();placePatrolBoat();}
 placeShipsGrid();
